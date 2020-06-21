4e5e98840485ec66df1c2c24129fc31c67eb9a37
Dmytro Golub <dmytro.golub@gmail.com>
2020-06-21 17:06:55 +0100

    Add incoming call

--- CallUI/ViewController.swift
+++ CallUI/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import CallKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CXProviderDelegate {
+    
+    let provider = CXProvider(configuration: CXProviderConfiguration(localizedName: "My App"))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        provider.setDelegate(self, queue: nil)
+    }
+    
+    func providerDidReset(_ provider: CXProvider) {
+        
+    }
+    
+    func provider(_ provider: CXProvider, perform action: CXAnswerCallAction) {
+        action.fulfill()
     }
 
+    func provider(_ provider: CXProvider, perform action: CXEndCallAction) {
+        action.fulfill()
+    }
+    @IBAction private func callDidInitialise(_ sender: Any) {
+        let update = CXCallUpdate()
+         update.remoteHandle = CXHandle(type: .generic, value: "The Mad Hatter")
+         provider.reportNewIncomingCall(with: UUID(), update: update, completion: { error in })
 
+    }
+    
+    @IBAction private func outgoingCallDidInitialise(_ sender: Any) {
+        let controller = CXCallController()
+        let transaction = CXTransaction(action: CXStartCallAction(call: UUID(), handle: CXHandle(type: .generic, value: "Alice")))
+        controller.request(transaction, completion: { error in })
+        
+        DispatchQueue.main.asyncAfter(wallDeadline: DispatchWallTime.now() + 5) {
+            self.provider.reportOutgoingCall(with: controller.callObserver.calls[0].uuid, connectedAt: nil)
+        }
+    }
 }
 
